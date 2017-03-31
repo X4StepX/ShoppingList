cc8d2910e53edc6bdf67be0ebf4051b6b7fce804
eduard <eduardbraun913gmail.com>
2017-03-30 20:18:09 -0500

    added shopping list adapter

--- app/src/main/java/com/example/eduard/shoppinglist/Adapters/ShoppingListAdapter.java
+++ app/src/main/java/com/example/eduard/shoppinglist/Adapters/ShoppingListAdapter.java
@@ -3,7 +3,9 @@ package com.example.eduard.shoppinglist.Adapters;
 import android.content.Context;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import com.example.eduard.shoppinglist.Models.ShopingList;
@@ -16,15 +18,50 @@ import java.util.ArrayList;
  * Created by eduard on 3/29/2017.
  */
 
-public class ShoppingListAdapter extends ArrayAdapter<ShopingList> {
-    public Context mContext;
-    public ArrayList<ShopingList> shopingLists;
+public class ShoppingListAdapter extends ArrayAdapter<ShopingList>{
+    private ArrayList<ShopingList> shopingLists;
+    Context mContext;
 
-    public ShoppingListAdapter(ArrayList<ShopingList> data, Context context) {
-        super(context, R.layout.shopping_list_item, data);
-        this.shopingLists = data;
-        this.mContext = context;
+    // View lookup cache
+    private static class ViewHolder {
 
+    }
+
+    public ShoppingListAdapter(ArrayList<ShopingList> shopingList, Context context) {
+        super(context, R.layout.shopping_list_item, shopingList);
+        this.shopingLists = shopingList;
+        this.mContext=context;
+
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        ShopingList shopingList = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        ViewHolder viewHolder; // view lookup cache stored in tag
+
+        final View result;
+
+        if (convertView == null) {
+
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.shopping_list_item, parent, false);
+
+
+            result=convertView;
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+            result=convertView;
+        }
+
+
+        // Return the completed view to render on screen
+        return convertView;
     }
 
 }
